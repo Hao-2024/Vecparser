ee806caac0d7a388f0a5c3a806c267a61e00f5b5
Hao-2024 <3582786053@qq.com>
2024-07-18 16:58:01 +0800

    Update loop_editor.m

--- loop_editor.m
+++ loop_editor.m
@@ -1,6 +1,6 @@
-        for ang1=1:90/ad+1
-            for ang2=1:360/ad+1
+for ang1=1:90/ad+1
+        for ang2=1:360/ad+1
                 aa=reshape(a(ang1,ang2,:),[kelm*kelm 1]);
                 SPangle(ang1,ang2)=(aa'*aa)/(aa'*REn*aa);
-            end 
-        end
+        end 
+end
