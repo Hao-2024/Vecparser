56b46dfe8424a88aa73345ff96e51cdd68487e1c
Hao-2024 <3582786053@qq.com>
2024-07-18 16:54:20 +0800

    my loop_editor.m

--- loop_editor.m
+++ loop_editor.m
@@ -1,23 +1,6 @@
-
-%example
-for n1=1:N1
-    for n2=1:N2
-    qq(n1,n2)= -(y(n1)+z(n2))*h(n2,n1);
-        for n3=1:N3
-            for n4=1:N4
-                if n1~=n2*n3 && n3>n4^3
-                    x(n1,n2,n3,n4)= (y(n1,n3)+z(n4))*h(n2,n3,n1);
-                    q(n4,n3,n2,n1)= -h(n2,n3,n1)+((y(n1,n3)+z(n4))*h(n2,n3,n1))^2;
-                end
-                    x(n1,n2,n3,n4)= (y(n1,n3)+z(n4))*h(n2,n3,n1);
-                    q(n4,n3,n2,n1)= -h(n2,n3,n1)+((y(n1,n3)+z(n4))*h(n2,n3,n1))^2;
-                if n1~=n2*n3 && n3>n4^3
-                    x(n1,n2,n3,n4)<= (y(n1,n3)+z(n4))*h(n2,n3,n1);
-                    q(n4,n3,n2,n1)>= -h(n2,n3,n1)+((y(n1,n3)+z(n4))*h(n2,n3,n1))^2;
-                end
-                    x(n1,n2,n3,n4)= (y(n1,n3)+z(n4))*h(n2,n3,n1);
-                    q(n4,n3,n2,n1)= -h(n2,n3,n1)+((y(n1,n3)+z(n4))*h(n2,n3,n1))^2;
-            end
+        for ang1=1:90/ad+1
+            for ang2=1:360/ad+1
+                aa=reshape(a(ang1,ang2,:),[kelm*kelm 1]);
+                SPangle(ang1,ang2)=(aa'*aa)/(aa'*REn*aa);
+            end 
         end
-    end
-end
